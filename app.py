267cf7e7840eeaa4f148d8e7113a6a543c8e955b
Dharshini Kannan <dharshinikannan@MacBook-Air.local>
2025-06-19 21:00:02 -0700

    Fix Error

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import os
 from recommender.recommender import CourseRecommender
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"], "allow_headers": ["Content-Type", "Authorization"]}})csv_path = 'new_cse.csv'
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"], "allow_headers": ["Content-Type", "Authorization"]}})
+csv_path = 'new_cse.csv'
 recommender = CourseRecommender(csv_path)
 
 @app.route('/api/health', methods=['GET'])
